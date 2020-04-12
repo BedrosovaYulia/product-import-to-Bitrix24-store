52178f42145058582a6be19668b0e4b84c663db2
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-04-12 17:02:50 +0300

    parser

--- Lambda/S3trigger/lambda_function.py
+++ Lambda/S3trigger/lambda_function.py
@@ -1,5 +1,11 @@
+import os
 import json
 import boto3
+from xml.dom import minidom
+
+DYNAMODB = boto3.resource('dynamodb','us-east-1')
+TABLE = DYNAMODB.Table('B24_products')
+        
 
 def lambda_handler(event, context):
     # TODO implement
@@ -18,7 +24,37 @@ def lambda_handler(event, context):
         
         fileContent=fileObj["Body"].read().decode('utf-8')
         
-        print(fileContent)
+        #parse xml
+        xmldoc = minidom.parseString(fileContent)
+        offers = xmldoc.getElementsByTagName('offer')
+        
+        k=0
+        for offer in offers:
+            if k<5 or os.environ['test_mode']=='off':
+                k+=1
+        
+                product_name = offer.getElementsByTagName("name")[0].firstChild.data
+                product_price = offer.getElementsByTagName("price")[0].firstChild.data
+                product_picture = offer.getElementsByTagName("picture")[0].firstChild.data
+        
+                attrs = dict(offer.attributes.items())
+        
+                product_id=attrs["id"]
+                product_available=attrs["available"]
+        
+                #save data in database
+    
+                item = {'id':int(product_id)}
+        
+                item['product_available']=product_available
+                item['product_name']=product_name
+                item['product_price']=product_price
+                item['product_picture']=product_picture
+        
+                TABLE.put_item(Item=item)
+                
+            else:
+                break
     
     return {
         'statusCode': 200,
