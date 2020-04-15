32b2e7aa71b1771129da16ee4abbad1ffabb3f27
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-04-15 12:04:33 +0300

    using B24 lib

--- Lambda/ProductsDBProcessing/lambda_function.py
+++ Lambda/ProductsDBProcessing/lambda_function.py
@@ -1,51 +1,15 @@
 import os
 import json
 import boto3
-import requests
-from urllib.parse import parse_qs
-import base64
+#Bitrix24 lib - in Layers
+from bitrix24 import Bitrix24
 
+domain=os.environ['domain']
+key=os.environ['key']
+        
+bx24 = Bitrix24(domain, user_id=1)
 
-
-def http_build_query(params, topkey = ''):
-  from urllib.parse import quote
- 
-  if len(params) == 0:
-    return ""
- 
-  result = ""
- 
-  # is a dictionary?
-  if type (params) is dict:
-    for key in params.keys():
-      newkey = quote (key)
-      if topkey != '':
-        newkey = topkey + quote('[' + key + ']')
- 
-      if type(params[key]) is dict:
-        result += http_build_query (params[key], newkey)
- 
-      elif type(params[key]) is list:
-        i = 0
-        for val in params[key]:
-          result += newkey + quote('[' + str(i) + ']') + "=" + quote(str(val)) + "&"
-          i = i + 1
- 
-      # boolean should have special treatment as well
-      elif type(params[key]) is bool:
-        result += newkey + "=" + quote (str(int(params[key]))) + "&"
- 
-      # assume string (integers and floats work well)
-      else:
-        result += newkey + "=" + quote (str(params[key])) + "&"
- 
-  # remove the last '&'
-  if (result) and (topkey == '') and (result[-1] == '&'):
-    result = result[:-1]
- 
-  return result
-
-def add_b24_product(key, name, price, file_url, xml_id):
+def add_b24_product(name, price, file_url, xml_id):
   
   import requests
   import base64
@@ -69,13 +33,12 @@ def add_b24_product(key, name, price, file_url, xml_id):
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['0']="1.png"
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['1']=image_64_encode
 
-  response = requests.post(key+"crm.product.add",http_build_query(product_data))
-  result=response.json()
+  result = bx24.call_webhook('crm.product.add', key, params=product_data)
   
   return result["result"]
   
   
-def update_b24_product(bitrix_id, key, name, price, file_url, xml_id):
+def update_b24_product(bitrix_id, name, price, file_url, xml_id):
   
   import requests
   import base64
@@ -103,8 +66,10 @@ def update_b24_product(bitrix_id, key, name, price, file_url, xml_id):
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['0']="1.png"
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['1']=image_64_encode
   
-  response = requests.post(key+"crm.product.update",http_build_query(product_data))
-  result=response.json()
+  #response = requests.post(key+"crm.product.update",http_build_query(product_data))
+  #result=response.json()
+  
+  result = bx24.call_webhook('crm.product.update', key, params=product_data)
   
   return result
     
@@ -123,8 +88,8 @@ def lambda_handler(event, context):
             
             #process the picture, put the result in the queue, where requests to B24 will accumulate
             #while for the test we’ll immediately make a request to Bitrix24
-            key=os.environ['B24key']
-            bitrix_id=add_b24_product(key, offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
+            
+            bitrix_id=add_b24_product(offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
             print("in Bitrix24 added product with ID: ",bitrix_id)
             
             #add Bitrix24 product id to the product table
@@ -150,8 +115,8 @@ def lambda_handler(event, context):
             
             #We update the price only if it has changed
             if offer["product_price"]["S"]!=offer_old["product_price"]["S"]:
-              key=os.environ['B24key']
-              result=update_b24_product(offer_old["bitrix_id"]["N"], key, offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
+              
+              result=update_b24_product(offer_old["bitrix_id"]["N"], offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
               print(result)
             
             #return bitrix_id to the place
