b8de059c1fd2069564568e5e6eca9b3aa3b91114
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-04-16 14:38:14 +0300

    add batching

--- Lambda/ProductsDBProcessing/lambda_function.py
+++ Lambda/ProductsDBProcessing/lambda_function.py
@@ -9,6 +9,12 @@ key=os.environ['key']
         
 bx24 = Bitrix24(domain, user_id=1)
 
+DYNAMODB = boto3.resource('dynamodb','us-east-1')
+TABLE = DYNAMODB.Table('B24_products')
+
+calls_add=dict()
+calls_update=dict()
+
 def add_b24_product(name, price, file_url, xml_id):
   
   import requests
@@ -32,10 +38,14 @@ def add_b24_product(name, price, file_url, xml_id):
         
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['0']="1.png"
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['1']=image_64_encode
-
-  result = bx24.call_webhook('crm.product.add', key, params=product_data)
   
-  return result["result"]
+  #collect the batch
+  calls_add[xml_id]={
+                'method': 'crm.product.add',
+                'params': product_data
+            }
+
+  return True
   
   
 def update_b24_product(bitrix_id, name, price, file_url, xml_id):
@@ -66,71 +76,82 @@ def update_b24_product(bitrix_id, name, price, file_url, xml_id):
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['0']="1.png"
   product_data["fields"]["PREVIEW_PICTURE"]["fileData"]['1']=image_64_encode
   
-  #response = requests.post(key+"crm.product.update",http_build_query(product_data))
-  #result=response.json()
-  
   result = bx24.call_webhook('crm.product.update', key, params=product_data)
   
   return result
     
-DYNAMODB = boto3.resource('dynamodb','us-east-1')
-TABLE = DYNAMODB.Table('B24_products')
-
 def lambda_handler(event, context):
     # TODO implement
     
     print(event)
     
+    calls_add.clear()
+    calls_update.clear()
+
+    
     for Record in event["Records"]:
-        if Record["eventName"]=="INSERT" and Record["dynamodb"]["NewImage"]["bitrix_id"]["S"]=='0':
+        #print(Record["eventName"])
+        #print(Record["dynamodb"]["NewImage"]["bitrix_id"]["N"])
+        if Record["eventName"]=="INSERT" and int(Record["dynamodb"]["NewImage"]["bitrix_id"]["N"])==0:
             
+            print("Зашло")
             offer=Record["dynamodb"]["NewImage"]
+            add_b24_product(offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
             
-            #process the picture, put the result in the queue, where requests to B24 will accumulate
-            #while for the test we’ll immediately make a request to Bitrix24
-            
-            bitrix_id=add_b24_product(offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
-            print("in Bitrix24 added product with ID: ",bitrix_id)
-            
-            #add Bitrix24 product id to the product table
-            response = TABLE.update_item(
-              Key={
-                  'id': int(offer["id"]["N"]),
-                },
-              UpdateExpression="set bitrix_id = :r",
-              ExpressionAttributeValues={
-                  ':r': bitrix_id,
-              },
-              ReturnValues="UPDATED_NEW"
-            )
-            
-        
         if Record["eventName"]=="MODIFY":
           
             offer=Record["dynamodb"]["NewImage"]
             offer_old=Record["dynamodb"]["OldImage"]
             
-            #process the picture, put the result in the queue, where requests to B24 will accumulate
-            #while for the test we’ll immediately make a request to Bitrix24
-            
             #We update the price only if it has changed
             if offer["product_price"]["S"]!=offer_old["product_price"]["S"]:
+              update_b24_product(offer_old["bitrix_id"]["N"], offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
               
-              result=update_b24_product(offer_old["bitrix_id"]["N"], offer["product_name"]["S"], offer["product_price"]["S"], offer["product_picture"]["S"], offer["id"]["N"])
-              print(result)
-            
             #return bitrix_id to the place
-            response = TABLE.update_item(
+            if int(offer_old["bitrix_id"]["N"])>0 and int(offer["bitrix_id"]["N"])==0:
+             response = TABLE.update_item(
+                Key={
+                    'id': int(offer["id"]["N"]),
+                 },
+               UpdateExpression="set bitrix_id = :r",
+               ExpressionAttributeValues={
+                    ':r': offer_old["bitrix_id"]["N"],
+               },
+               ReturnValues="UPDATED_NEW"
+             )
+    
+    
+    #execute a batch request for all additions
+    if len(calls_add)>0:
+      print(len(calls_add))
+      
+      result = bx24.call_batch_webhook(calls_add, key, True)
+      print(result['result']['result'])
+      
+      #add Bitrix24 product id to the product table
+      for Record in event["Records"]:
+        if Record["eventName"]=="INSERT" and int(Record["dynamodb"]["NewImage"]["bitrix_id"]["N"])==0:
+          #print(Record)
+          offer=Record["dynamodb"]["NewImage"]
+          print(offer["id"]["N"])
+          print(result['result']['result'][offer["id"]["N"]])
+          response = TABLE.update_item(
               Key={
                   'id': int(offer["id"]["N"]),
                 },
               UpdateExpression="set bitrix_id = :r",
               ExpressionAttributeValues={
-                  ':r': offer_old["bitrix_id"]["N"],
+                  ':r': result['result']['result'][offer["id"]["N"]],
               },
               ReturnValues="UPDATED_NEW"
             )
-        
+    
+    
+    #execute a batch request for all updates
+    if len(calls_update)>0:
+      result = bx24.call_batch_webhook(calls_update, key, True)
+      #print(result['result'])
+    
             
     return {
         'statusCode': 200,
